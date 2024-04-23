4c1e269d198a05ec8237a002b6fdc59cd2e3fbc2
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-04-23 11:38:52 +0200

    Switched to stable for scylladb

--- server/flake.nix
+++ server/flake.nix
@@ -32,8 +32,8 @@
         nixpkgs.legacyPackages.aarch64-linux.proot
         nix_unstable.legacyPackages.x86_64-linux.redict
         nix_unstable.legacyPackages.aarch64-linux.redict
-        nix_unstable.legacyPackages.x86_64-linux.scylladb
-        nix_unstable.legacyPackages.aarch64-linux.scylladb
+        nixpkgs.legacyPackages.x86_64-linux.scylladb
+        nixpkgs.legacyPackages.aarch64-linux.scylladb
       ];
     };
   };
