679edb27d4fd250d7f27d3e196b13457ed910c65
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-04-24 13:35:21 +0200

    Added phoronix-test-suite

--- server/zune-image/crates/zune-inflate/fuzz/fuzz_targets/decode_zlib.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    let mut decoder = zune_inflate::DeflateDecoder::new(data);
-    let _result = decoder.decode_zlib();
-});
--- server/zune-image/fuzz-corpus/fuzz.sh
+++
@@ -1,9 +0,0 @@
-# Move one directory up.
-cd ..
-
-echo "Running the PNG FUZZER"
-cargo +nightly fuzz  run  --fuzz-dir zune-png/fuzz decode_buffer  fuzz-corpus/png -- -runs=100000
-
-
-echo "Running the JPEG FUZZER"
-cargo +nightly fuzz run --fuzz-dir zune-jpeg/fuzz decode_buffer -- -runs=10000 zune-jpeg/test-images/fuzz_references
